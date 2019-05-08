7b6b5c757ad6af347c5181fb2a2198d91efdef17
rumyantsevat <ipochinili@gmail.com>
2019-05-08 14:19:15 +0300

    initialization repo

---
+++ migrations/m190508_102320_create_article_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%article}}`.
+ */
+class m190508_102320_create_article_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%article}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%article}}');
+    }
+}
---
+++ migrations/m190508_102415_create_category_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m190508_102415_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%category}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%category}}');
+    }
+}
